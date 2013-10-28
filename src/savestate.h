c4f2e7d310daa5897be6a58de984bef8bc7c1e76
Christopher Morton <christopherdavidmorton@gmail.com>
2013-10-28 01:54:38 -0500

    Compile/link my libs as a static library

--- src/savestate.h
+++ src/savestate.h
@@ -5,9 +5,9 @@
 #define LR_EASY 0x1
 #define LR_EASY_RT 0x2
 #define LR_EASY_MM 0x4
-#define LR_HARD 0x8
-#define LR_HARD_RT 0x10
-#define LR_HARD_MM 0x20
+#define LR_HARD 0x10
+#define LR_HARD_RT 0x20
+#define LR_HARD_MM 0x40
 
 namespace sum
 {
